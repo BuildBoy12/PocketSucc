89b1cd96cdb4e4592d8517ef6f2cbb1788114373
Build <jwbradley2003@gmail.com>
2022-04-13 01:47:04 -0400

    Bump to Exiled 5.1.3

--- PocketSucc/Plugin.cs
+++ PocketSucc/Plugin.cs
@@ -31,7 +31,7 @@ namespace PocketSucc
         public override string Author => "Build";
 
         /// <inheritdoc/>
-        public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(5, 1, 3);
 
         /// <inheritdoc/>
         public override Version Version { get; } = new Version(1, 0, 0);
